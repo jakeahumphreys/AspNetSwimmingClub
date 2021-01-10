646411fb41efbd6f2ca3961ea2483d5ae2e1c328
Jake H <jakehumphreys@outlook.com>
2021-01-10 19:57:38 +0000

    Cleanup

---
+++ MVCWebAssignment1/Customisations/CustomHandleErrorAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCWebAssignment1.Customisations
+{
+    public class CustomHandleErrorAttribute : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+            filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Error", action = "Error", ErrorType = ErrorType.Service, Message = filterContext.Exception.Message }));
+        }
+    }
+}
