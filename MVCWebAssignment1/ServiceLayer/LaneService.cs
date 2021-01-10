1d25a4153896edfcee028c80a96bb99e048a7e04
Jake H <jakehumphreys@outlook.com>
2021-01-10 16:34:55 +0000

    FamilyGroup logic delegated to service

---
+++ MVCWebAssignment1/ServiceLayer/LaneService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebAssignment1.ServiceLayer
+{
+    public class LaneService
+    {
+    }
+}
